c9ecba60daf5ff5700a2570a6965f2af7530ad8c
mateo <mateo.ortiz54@misena.edu.co>
2022-09-28 09:39:00 -0500

    probando desde sena

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -2,15 +2,11 @@
 function validar(params) {
   window.location.href = params
 }
-
-
-
 function eliminar(params){
 
   return confirm('Desea Continuar Con la operacion?') ? window.location.href=params : console.log('operacion Abortada')
 
 };
-
 
 $(document).ready(function () {
   $('#example').DataTable({
